943dd62b8dc569fd13adb7f082276d4d776bff80
Kaschmir <155475773+Kaschmir@users.noreply.github.com>
2024-01-04 19:35:14 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# IPWA-CO2
+# CO² Footprint
+
+"npm install"
+"npm run dev"
+
+```
+ipwa01-co2
+├─ .gitignore
+├─ index.html
+├─ package-lock.json
+├─ package.json
+├─ postcss.config.js
+├─ public
+│  └─ logo.png
+├─ README.md
+├─ src
+│  ├─ App.vue
+│  ├─ assets
+│  │  └─ data.json
+│  ├─ components
+│  │  ├─ Footer.vue
+│  │  ├─ Navbar.vue
+│  │  └─ Table.vue
+│  ├─ main.js
+│  ├─ pages
+│  │  ├─ Datenschutz.vue
+│  │  ├─ Help.vue
+│  │  ├─ Home.vue
+│  │  └─ Impressum.vue
+│  ├─ router
+│  │  └─ index.js
+│  └─ style.css
+├─ tailwind.config.js
+└─ vite.config.js
+
+```
---
+++ src/pages/Impressum.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-full p-10 text-center">
+      <h1 class="text-2xl font-bold">Impressum</h1>
+      <div class="px-5 rounded overflow-hidden shadow-lg">
+        <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+         <p>WORDOF KLIMAWANDELt<br />
+        Rotherstra&szlig;e 25<br />
+        10245 Berlin</p>
+
+        <p><strong>Vertreten durch:</strong><br />
+        Hannah Lorenz
+        Luise Beispiel</p>
+
+        <h2>Kontakt</h2>
+        <p>Telefon: +49 30 1556897 - 20<br />
+        Telefax: +49 30 1556897 - 21<br />
+        E-Mail: H.Lorenz@wordofklimawandel.de</p>
+
+        <h2>Umsatzsteuer-ID</h2>
+        <p>Umsatzsteuer-Identifikationsnummer gem&auml;&szlig; &sect; 27 a Umsatzsteuergesetz:<br />
+        DE159806501</p>
+
+        <h2>EU-Streitschlichtung</h2>
+        <p>Online-Streitbeilegung-Plattform der Europ&auml;ische Kommission: <a href="https://ec.europa.eu/consumers/odr/" target="_blank" rel="noopener noreferrer">https://ec.europa.eu/consumers/odr/</a>.<br />
+
+        <p> -</p>
+      </div>
+    </div>
+</template>
